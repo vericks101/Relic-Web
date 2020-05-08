b169d4419e34c26e7e6191cea7b6a03e2ddd03ad
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2020-05-08 11:52:35 -0700

    Added Placeholder Nav Logo.

--- ekodex-web/src/components/ui/NavigationBar.js
+++ ekodex-web/src/components/ui/NavigationBar.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import {Nav, Navbar} from 'react-bootstrap';
-// import styled from 'styled-components';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
 import LoginComponent from '../LoginComponent';
 import '../../index.css';
 
 
-// const Styles = styled.div`
-//     .navbar {
-//         background-color: #222;
-//     }
-
-//     .navbar-brand, .navbar-nav .nav-link, .navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .show .nav-link, .navbar-light .navbar-nav .nav-link:focus {
-//         color: #bbb;
-
-//         &:hover {
-//             color: white;
-//         }
-//     }
-// `;
+const Styles = styled.div`
+    .navLogo {
+        margin-bottom: 5px;
+    }
+`;
 
 export const NavigationBar = () => (
+    <Styles>
     <Navbar className="nav-background" collapseOnSelect expand="lg" sticky="top">
-    <Navbar.Brand href="/">EKODEX</Navbar.Brand>
+    <Navbar.Brand href="/">
+        <img
+            alt=""
+            src={require("../../assets/grey.jpg")}
+            width="30"
+            height="30"
+            className="navLogo"
+        />{' '}
+        EKODEX
+      </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
@@ -29,18 +31,5 @@ export const NavigationBar = () => (
         </Nav>
     </Navbar.Collapse>
     </Navbar>
-    // <Styles>
-    //     <Navbar expand='lg'>
-    //         <Navbar.Brand href='/'>Ekodex</Navbar.Brand>
-    //         <Navbar.Toggle area-controls='basic-navbar-nav'/>
-    //         <Navbar.Collapse id='basic-navbar-nav'>
-    //             <Nav className = 'ml-auto'>
-    //                 <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
-    //                 <Nav.Item><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
-    //                 <Nav.Item><Nav.Link href='/contact'>Contact</Nav.Link></Nav.Item>
-    //                 <Nav.Item><LoginComponent/></Nav.Item>
-    //             </Nav>
-    //         </Navbar.Collapse>
-    //     </Navbar>
-    // </Styles>
+    </Styles>
 )
